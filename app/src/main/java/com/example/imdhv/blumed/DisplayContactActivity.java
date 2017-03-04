40e9e3933f33b8c2611e3338ff48cac66b79168b
imdhv <14ceuos041.ce@ddu.ac.in>
2017-03-04 14:02:36 +0530

    Optimized

--- app/src/main/java/com/example/imdhv/blumed/DisplayContactActivity.java
+++ app/src/main/java/com/example/imdhv/blumed/DisplayContactActivity.java
@@ -1,7 +1,9 @@
 package com.example.imdhv.blumed;
 
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -25,55 +27,85 @@ public class DisplayContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_contact);
         lv = (ListView) findViewById(R.id.lv1);
-        arrlistnames.clear();
+        new MyTask().execute();
+    }
 
-
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                null, null, null, null);
-
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
-                String id = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME));
-
-                if (cur.getInt(cur.getColumnIndex(
-                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
-                            new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
-                       // Toast.makeText(this, "Name: " + name
-                         //       + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
-                        //arrlistnames.add(name.replaceAll("\\s+","").trim());
-                        String p = phoneNo.replaceAll("\\s+","").replaceAll("-","").trim();
-                        if(p.length() >= 10) {
-                            if (!arrlistphonenumbers.contains(p.substring(p.length() - 10))) {
-                                arrlistnames.add(name);
-                                arrlistphonenumbers.add(p.substring(p.length() - 10));
-                            }
-                        }
-                    }
-                    pCur.close();
-                }
-            }
+    class MyTask extends AsyncTask<String, String, String>{
+        ProgressDialog pd;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd= new ProgressDialog(DisplayContactActivity.this);
+            pd.setIndeterminate(true);
+            pd.setCancelable(false);
+            pd.setTitle("Loading...");
+            pd.setMessage("Please Wait...");
+            pd.show();
         }
 
-        //arrlistphonenumbers.add(Integer.toString(arrlistnames.size()));
-        //arrlistphonenumbers.add(Integer.toString(arrlistphonenumbers.size()));
-        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, arrlistphonenumbers);
-        lv.setAdapter(aa);
-        //String json = new Gson().toJson(arrlistphonenumbers);
-        JSONArray jsArray = new JSONArray(arrlistphonenumbers);
-        String s = jsArray.toString();
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(pd!=null){
+                pd.dismiss();
+            }
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(DisplayContactActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, arrlistphonenumbers);
+            lv.setAdapter(aa);
+        }
 
+        @Override
+        protected String doInBackground(String... params) {
+            ContentResolver cr = getContentResolver();
+            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null, null);
 
+            if (cur.getCount() > 0) {
+                while (cur.moveToNext()) {
+                    String id = cur.getString(
+                            cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cur.getString(cur.getColumnIndex(
+                            ContactsContract.Contacts.DISPLAY_NAME));
 
+                    if (cur.getInt(cur.getColumnIndex(
+                            ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                        Cursor pCur = cr.query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                                new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            // Toast.makeText(this, "Name: " + name
+                            //       + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+                            //arrlistnames.add(name.replaceAll("\\s+","").trim());
+                            String p = phoneNo.replaceAll("\\s+","").replaceAll("-","").trim();
+                            if(p.length() >= 10) {
+                                if (!arrlistphonenumbers.contains(p.substring(p.length() - 10))) {
+                                    arrlistnames.add(name);
+                                    arrlistphonenumbers.add(p.substring(p.length() - 10));
+                                }
+                            }
+                        }
+                        pCur.close();
+                    }
+                }
+            }
+
+            //arrlistphonenumbers.add(Integer.toString(arrlistnames.size()));
+            //arrlistphonenumbers.add(Integer.toString(arrlistphonenumbers.size()));
+
+            //String json = new Gson().toJson(arrlistphonenumbers);
+            JSONArray jsArray = new JSONArray(arrlistphonenumbers);
+            String s = jsArray.toString();
+            return null;
+        }
     }
+
+
+
+
+
+
+
 }
